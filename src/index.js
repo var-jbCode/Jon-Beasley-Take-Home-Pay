e2364b0ffbdb5409863bf433de98d3e99026b663
Jon Beasley <jonbeas@gmail.com>
2021-06-01 17:49:36 +0100

    Added styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TaxApp from './TaxApp';
-
+import TaxApp from './components/TaxApp';
+import './styles/styles.scss'
 
 ReactDOM.render(
   <React.StrictMode>
