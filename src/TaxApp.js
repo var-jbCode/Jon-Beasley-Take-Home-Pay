46248a71e54f65633dc71373aa81c499b461aa98
Jon Beasley <jonbeas@gmail.com>
2021-06-01 16:57:46 +0100

    added input validation

--- src/TaxApp.js
+++ src/TaxApp.js
@@ -7,48 +7,53 @@ const TaxApp = () => {
     const [annualNIDue, setannualNIDue] = useState()
     const [takeHome, setTakeHome] = useState()
     const [resultsVisible, setResultsVisisble] = useState()
+    const [inputError, setInputError] = useState(false)
 
 
     return (
         <div>
             <div>
                 <h1>Your "Take Home" Pay Calculator</h1>
+                {inputError && <p>Please only enter a integer salary without comma seperation :)</p>}
                 £ <input
                     type="text"
-                    min="0.01"
-                    step="0.01"
                     name="salaryInput"
                     id="salaryInput"
-                    pattern="[0-9]*"
                     placeholder="Put your Salary in here!"
                     onChange={(e) => setSalary(e.target.value)}
                 />
                 <button
                     onClick={() => {
-                        console.log(salary)
-                        if (salary < 15000) {
-                            setTakeHome(salary)
-                            setannualNIDue(0)
-                            setannualTaxDue(0)
-                            setResultsVisisble(true)
-                        } else if (salary > 15000 && salary < 50000) {
-                            let taxFree = 15000
-                            let midTax = (salary - taxFree) * 0.2
-                            let midNI = (salary - taxFree) * 0.12
-                            setTakeHome(salary - midTax - midNI)
-                            setannualNIDue(midNI)
-                            setannualTaxDue(midTax)
-                            setResultsVisisble(true)
-                        } else if (salary > 50000) {
-                            let aboveFiftyK = salary - 50000
-                            let topTax = 7000 + (aboveFiftyK * 0.4)
-                            let topNI = 4200 + (aboveFiftyK * 0.02)
-                            setTakeHome(salary - topTax - topNI)
-                            setannualNIDue(topNI)
-                            setannualTaxDue(topTax)
-                            setResultsVisisble(true)
-                        }
+                        if (salary.match(/\d+/)) {
+                            setInputError(false)
+                            if (salary < 15000) {
+                                setTakeHome(salary)
+                                setannualNIDue(0)
+                                setannualTaxDue(0)
+                                setResultsVisisble(true)
+                            } else if (salary > 15000 && salary < 50000) {
+                                let taxFree = 15000
+                                let midTax = (salary - taxFree) * 0.2
+                                let midNI = (salary - taxFree) * 0.12
+                                setTakeHome(salary - midTax - midNI)
+                                setannualNIDue(midNI)
+                                setannualTaxDue(midTax)
+                                setResultsVisisble(true)
+                            } else if (salary > 50000) {
+                                let aboveFiftyK = salary - 50000
+                                let topTax = 7000 + (aboveFiftyK * 0.4)
+                                let topNI = 4200 + (aboveFiftyK * 0.02)
+                                setTakeHome(salary - topTax - topNI)
+                                setannualNIDue(topNI)
+                                setannualTaxDue(topTax)
+                                setResultsVisisble(true)
+                            }
 
+                        } else {
+                            setInputError(true)
+                            setResultsVisisble(false)
+
+                        }
                     }}
 
 
