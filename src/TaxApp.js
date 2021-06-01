a6bb479c6c5c1d6b87a4c4f5173ac171c096ccba
Jon Beasley <jonbeas@gmail.com>
2021-06-01 15:31:36 +0100

    initial commit

---
+++ src/TaxApp.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+
+const TaxApp = () => {
+    const [salary, setSalary] = useState()
+
+    return (
+        <section>
+            <h1>Your "Take Home" Pay Calculator</h1>
+            <input
+                type="text"
+                name="salaryInput"
+                id="salaryInput"
+                placeholder="Put your Salary in here!"
+                onChange={(e) => setSalary(e.target.value)}
+            />
+            <button
+                onClick={() => console.log(salary)}
+            >Calculate Take Home Pay</button>
+
+
+        </section>
+    );
+}
+
+export default TaxApp;
