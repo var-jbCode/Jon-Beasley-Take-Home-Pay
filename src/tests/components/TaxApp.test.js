52f16dd004cf614fec556f01a6ce0c859719cbeb
Jon Beasley <jon_beasley@ajg.com>
2021-09-06 16:35:59 +0100

    starting testing

--- src/tests/components/TaxApp.test.js
+++ src/tests/components/TaxApp.test.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import Enzyme, { mount } from 'enzyme';
+import Enzyme, { mount, ReactWrapper, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 import TaxApp from '../../components/TaxApp';
 
-describe(TaxApp, () => {
-	it('works :D', () => {
+describe('TaxApp', () => {
+	beforeEach(() => {
+		wrapper = shallow(<TaxApp />);
+	});
+
+	it('control test format', () => {
 		expect(2 + 2).toEqual(4);
 	});
+
+	it('renders correctly', () => {
+		shallow(<TaxApp />);
+	});
+
+	it('has one input', () => {
+		expect(wrapper.find('input').length).toEqual(1);
+	});
 });
+
+// https://www.youtube.com/watch?v=9lkZ77m-39I
